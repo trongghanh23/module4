8f6565b8d87209ba9f7853c7739fb66cfd9c8c87
trongghanh23 <truongggh1602@gmail.com>
2022-08-16 20:16:21 +0700

    nộp bài

--- 02_spring_controller/exercise/calculator/src/main/java/calculator/controller/CalculatorController.java
+++ 02_spring_controller/exercise/calculator/src/main/java/calculator/controller/CalculatorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class Calculator {
+public class CalculatorController {
     @Autowired
     private ICalculatorService iCalculatorService;
 
